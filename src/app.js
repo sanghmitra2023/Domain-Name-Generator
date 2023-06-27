087faf467a9242d3545904ed0a1f4c427cc81df9
sanghmitra2023 <128592793+sanghmitra2023@users.noreply.github.com>
2023-06-27 10:12:07 +0000

    final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const prefix = ["the", "my", "our"];
+const adjectives = ["cool", "amazing", "bestever", "super"];
+const noun = ["website", "blog", "domain", "app"];
+const tld = [".com", ".org", ".net", ".biz"];
+for (let i = 0; i < prefix.length; i++) {
+  for (let j = 0; j < adjectives.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let m = 0; m < tld.length; m++) {
+        console.log(`${prefix[i]}${adjectives[j]}${noun[k]}${tld[m]}`);
+      }
+    }
+  }
+}
